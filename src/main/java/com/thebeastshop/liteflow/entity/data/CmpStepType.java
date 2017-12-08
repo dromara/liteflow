98d7d2a924f52f2c3b8192643be3410a48031dc2
bryan.zhang <yuancheng.zhang@thebeastshop.com>
2017-12-08 17:32:27 +0800

    小修正

--- src/main/java/com/thebeastshop/liteflow/entity/data/CmpStepType.java
+++ src/main/java/com/thebeastshop/liteflow/entity/data/CmpStepType.java
@@ -1,9 +1,11 @@
 /**
- * <p>Title: beast-price</p>
- * <p>Description: 价格计算服务</p>
+ * <p>Title: liteFlow</p>
+ * <p>Description: 轻量级的组件式流程框架</p>
  * <p>Copyright: Copyright (c) 2017</p>
  * @author Bryan.Zhang
- * @Date 2017年12月8日
+ * @email weenyc31@163.com
+ * @Date 2017-12-8
+ * @version 1.0
  */
 package com.thebeastshop.liteflow.entity.data;
 
