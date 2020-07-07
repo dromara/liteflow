286d42d16e8934d55cd3947f7ed17e0de41bb983
bryan.zhang <yuancheng.zhang@thebeastshop.com>
2020-07-07 12:15:09 +0800

    文档更新，增加示例的说明和链接

--- liteflow-test-springboot/src/main/java/com/yomahub/flowtest/Runner.java
+++ liteflow-test-springboot/src/main/java/com/yomahub/flowtest/Runner.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlowtestApplication {
+public class Runner {
 
     public static void main(String[] args) {
         try{
-            SpringApplication.run(FlowtestApplication.class, args);
+            SpringApplication.run(Runner.class, args);
 
             System.exit(0);
         }catch (Throwable t){
