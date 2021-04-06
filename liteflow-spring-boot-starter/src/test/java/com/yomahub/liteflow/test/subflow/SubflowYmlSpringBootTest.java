7d385e02a13eb09c4b94ce474a0df76cc71e228e
徐佳 <xujia@xforceplus.com>
2021-04-06 16:38:38 +0800

    support #I3CT01 帮liteflow增加单元测试

---
+++ liteflow-spring-boot-starter/src/test/java/com/yomahub/liteflow/test/subflow/SubflowYmlSpringBootTest.java
@@ -0,0 +1,42 @@
+package com.yomahub.liteflow.test.subflow;
+
+import com.yomahub.liteflow.core.FlowExecutor;
+import com.yomahub.liteflow.entity.data.LiteflowResponse;
+import com.yomahub.liteflow.entity.data.Slot;
+import com.yomahub.liteflow.test.parser.LFParserXmlSpringbootTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * 测试显示调用子流程(yml)
+ * 单元测试
+ *
+ * @author justin.xu
+ */
+@RunWith(SpringRunner.class)
+@TestPropertySource(value = "classpath:/subflow/application-yml.properties")
+@SpringBootTest(classes = SubflowYmlSpringBootTest.class)
+@EnableAutoConfiguration
+@ComponentScan({"com.yomahub.liteflow.test.subflow.cmp1"})
+public class SubflowYmlSpringBootTest {
+    @Resource
+    private FlowExecutor flowExecutor;
+
+    //是否按照流程定义配置执行
+    @Test
+    public void testExplicitSubFlowYml() throws Exception {
+        LiteflowResponse<Slot> response = flowExecutor.execute("chain1", "it's a request");
+        Assert.assertTrue(response.isSuccess());
+        Assert.assertEquals("a==>b==>c==>b==>a", response.getData().printStep());
+    }
+
+
+}
