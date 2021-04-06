cd16006b2043eb09736e6265d17f2a478a0629b3
zendwang <wangzhenxian0556@126.com>
2021-04-07 06:08:27 +0800

    新增部分单元测试

--- liteflow-spring-boot-starter/src/test/java/com/yomahub/liteflow/test/config/LiteflowConfigNoSpringTest.java
+++ liteflow-spring-boot-starter/src/test/java/com/yomahub/liteflow/test/config/LiteflowConfigNoSpringTest.java
@@ -1,9 +1,12 @@
 package com.yomahub.liteflow.test.config;
 
 import com.yomahub.liteflow.core.FlowExecutor;
+import com.yomahub.liteflow.entity.data.LiteflowResponse;
+import com.yomahub.liteflow.entity.data.Slot;
 import com.yomahub.liteflow.exception.ConfigErrorException;
 import com.yomahub.liteflow.property.LiteflowConfig;
 import com.yomahub.liteflow.test.BaseTest;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -19,14 +22,22 @@ public class LiteflowConfigNoSpringTest extends BaseTest {
         executor.init();
     }
     
-    @Test(expected = ConfigErrorException.class)
-    public void testConfigError() throws Exception {
-        LiteflowConfig liteflowConfig = new LiteflowConfig();
-        liteflowConfig.setRuleSource("json:127.0.0.1:2181:/");
-        
+    @Test
+    public void testConfig() throws Exception {
         FlowExecutor executor = new FlowExecutor();
-        executor.setLiteflowConfig(liteflowConfig);
+        LiteflowConfig config = new LiteflowConfig();
+        config.setRuleSource("config/flow.json");
+        executor.setLiteflowConfig(config);
         executor.init();
+        LiteflowResponse<Slot> response = executor.execute("chain1", "arg");
+        Assert.assertTrue(response.isSuccess());
+        Assert.assertEquals(15, config.getWhenMaxWaitSeconds().intValue());
+        Assert.assertEquals(200, config.getQueueLimit().intValue());
+        Assert.assertEquals(300000L, config.getDelay().longValue());
+        Assert.assertEquals(300000L, config.getPeriod().longValue());
+        Assert.assertFalse(config.getEnableLog());
+        Assert.assertEquals(Runtime.getRuntime().availableProcessors() * 2, config.getWhenMaxWorkers().longValue());
+        Assert.assertEquals(512, config.getWhenQueueLimit().longValue());
     }
     
 }
