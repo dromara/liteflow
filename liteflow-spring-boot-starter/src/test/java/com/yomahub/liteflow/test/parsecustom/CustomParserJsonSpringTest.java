1beab89c760223dec78b1acdb311ff127b225902
dongguo.tao <dongguo.tao@china.zhaogang.com>
2021-04-07 15:31:51 +0800

    spring环境下的自定义配置源功能测试

--- liteflow-spring-boot-starter/src/test/java/com/yomahub/liteflow/test/parsecustom/CustomParserJsonSpringTest.java
+++ liteflow-spring-boot-starter/src/test/java/com/yomahub/liteflow/test/parsecustom/CustomParserJsonSpringTest.java
@@ -7,30 +7,24 @@ import com.yomahub.liteflow.test.BaseTest;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
 /**
- * springboot环境的自定义json parser单元测试
+ * spring环境的自定义json parser单元测试
  * @author dongguo.tao
  * @since 2.5.0
  */
 @RunWith(SpringRunner.class)
-@TestPropertySource(value = "classpath:/parsecustom/application.properties")
-@SpringBootTest(classes = CustomParserJsonSpringbootTest.class)
-@EnableAutoConfiguration
-@ComponentScan({"com.yomahub.liteflow.test.parsecustom.cmp"})
-public class CustomParserJsonSpringbootTest extends BaseTest {
+@ContextConfiguration("classpath:/parsecustom/application.xml")
+public class CustomParserJsonSpringTest extends BaseTest {
 
     @Resource
     private FlowExecutor flowExecutor;
 
-    //测试springboot场景的自定义json parser
+    //测试spring场景的自定义json parser
     @Test
     public void testSpringboot() throws Exception{
         LiteflowResponse<Slot> response = flowExecutor.execute("chain1", "args");
