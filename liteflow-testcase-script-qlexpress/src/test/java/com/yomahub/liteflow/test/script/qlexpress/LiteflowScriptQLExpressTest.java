b4f5aec504c221eef21457cf034e8d5f263c0e3d
bryan31 <weenyc31@163.com>
2021-08-12 17:31:46 +0800

    优化测试用例

--- liteflow-testcase-script-qlexpress/src/test/java/com/yomahub/liteflow/test/script/qlexpress/LiteflowScriptQLExpressTest.java
+++ liteflow-testcase-script-qlexpress/src/test/java/com/yomahub/liteflow/test/script/qlexpress/LiteflowScriptQLExpressTest.java
@@ -44,6 +44,6 @@ public class LiteflowScriptQLExpressTest extends BaseTest {
     public void testScript2() {
         LiteflowResponse<DefaultSlot> response = flowExecutor.execute2Resp("chain2", "arg");
         Assert.assertTrue(response.isSuccess());
-        Assert.assertEquals("d==>s2==>b", response.getSlot().printStep());
+        Assert.assertEquals("d==>s2[条件脚本]==>b", response.getSlot().printStep());
     }
 }
