a58e3da8db7a329c16b30ad48c1d79e36e2b3e4d
daiqi <466608943@qq.com>
2022-01-18 01:19:28 +0800

    测试

--- liteflow-testcase-springboot/src/test/java/com/yomahub/liteflow/test/privateDelivery/cmp/ACmp.java
+++ liteflow-testcase-springboot/src/test/java/com/yomahub/liteflow/test/privateDelivery/cmp/ACmp.java
@@ -8,23 +8,36 @@
 package com.yomahub.liteflow.test.privateDelivery.cmp;
 
 import com.yomahub.liteflow.annotation.LiteflowComponent;
+import com.yomahub.liteflow.core.FlowExecutor;
 import com.yomahub.liteflow.core.NodeComponent;
+import com.yomahub.liteflow.entity.data.DefaultSlot;
 import com.yomahub.liteflow.entity.data.Slot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @LiteflowComponent("a")
 public class ACmp extends NodeComponent {
-
+	@Autowired
+	private FlowExecutor flowExecutor;
 	@Override
 	public void process() {
 		System.out.println("ACmp executed!");
 		Slot slot = getSlot();
 		slot.setData("testSet", new HashSet<>());
 
-		for (int i = 0; i < 100; i++) {
-			this.sendPrivateDeliveryData("b",i+1);
+		try {
+			Queue<Integer> queue = new ConcurrentLinkedQueue<>();
+			for (int i = 1; i <= 100; i++) {
+				queue.add(i);
+			}
+            flowExecutor.execute2Resp("chain2", queue);
+
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
--- liteflow-testcase-springboot/src/test/java/com/yomahub/liteflow/test/privateDelivery/cmp/BCmp.java
+++ liteflow-testcase-springboot/src/test/java/com/yomahub/liteflow/test/privateDelivery/cmp/BCmp.java
@@ -1,26 +1,42 @@
 /**
  * <p>Title: liteflow</p>
  * <p>Description: 轻量级的组件式流程框架</p>
+ *
  * @author Bryan.Zhang
  * @email weenyc31@163.com
  * @Date 2020/4/1
  */
 package com.yomahub.liteflow.test.privateDelivery.cmp;
 
+import cn.hutool.core.collection.CollUtil;
 import com.yomahub.liteflow.annotation.LiteflowComponent;
 import com.yomahub.liteflow.core.NodeComponent;
 import org.springframework.stereotype.Component;
 
+import java.util.Queue;
 import java.util.Set;
 
 @LiteflowComponent("b")
 public class BCmp extends NodeComponent {
 
-	@Override
-	public void process() {
-		System.out.println("BCmp executed!");
-		Integer value = this.getPrivateDeliveryData();
-		Set<Integer> testSet = this.getSlot().getData("testSet");
-		testSet.add(value);
-	}
+    @Override
+    public boolean isAccess() {
+        Queue<Integer> values = this.getSlot().getRequestData();
+        System.out.println("BCmp executed! values.size" + values.size());
+        if (CollUtil.isEmpty(values)) {
+            return false;
+        }
+        Integer value = values.poll();
+        if (value == null) {
+            return false;
+        }
+        this.sendPrivateDeliveryData(this.getNodeId(), value);
+        return true;
+    }
+
+    @Override
+    public void process() {
+        Integer value = getPrivateDeliveryData();
+        System.out.println("BCmp executed!" + value);
+    }
 }
