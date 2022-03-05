9aeb10d40f90e0c5351db92ecf4667814aedb4fb
bryan31 <weenyc31@163.com>
2022-03-05 23:56:25 +0800

    补充测试用例

---
+++ liteflow-testcase-nospring/src/test/java/com/yomahub/liteflow/test/nodeExecutor/CustomerNodeExecutor.java
@@ -0,0 +1,20 @@
+package com.yomahub.liteflow.test.nodeExecutor;
+
+import com.yomahub.liteflow.core.NodeComponent;
+import com.yomahub.liteflow.entity.data.DataBus;
+import com.yomahub.liteflow.entity.data.Slot;
+import com.yomahub.liteflow.entity.executor.NodeExecutor;
+
+/**
+ * 自定义节点执行器
+ */
+public class CustomerNodeExecutor extends NodeExecutor {
+    @Override
+    public void execute(NodeComponent instance) throws Exception {
+        Slot slot = DataBus.getSlot(instance.getSlotIndex());
+        LOG.info("使用customerNodeExecutor进行执行");
+        slot.setData("customerNodeExecutor", this.getClass());
+        super.execute(instance);
+    }
+
+}
