70f2dfd41820b2827fb254929c15bd989e768dce
bryan31 <weenyc31@163.com>
2022-03-07 00:02:39 +0800

    补充测试用例

---
+++ liteflow-testcase-nospring/src/test/java/com/yomahub/liteflow/test/privateDelivery/cmp/ACmp.java
@@ -0,0 +1,28 @@
+/**
+ * <p>Title: liteflow</p>
+ * <p>Description: 轻量级的组件式流程框架</p>
+ * @author Bryan.Zhang
+ * @email weenyc31@163.com
+ * @Date 2020/4/1
+ */
+package com.yomahub.liteflow.test.privateDelivery.cmp;
+
+import com.yomahub.liteflow.core.NodeComponent;
+import com.yomahub.liteflow.entity.data.Slot;
+
+import java.util.HashSet;
+
+public class ACmp extends NodeComponent {
+
+	@Override
+	public void process() {
+		System.out.println("ACmp executed!");
+		Slot slot = getSlot();
+		slot.setData("testSet", new HashSet<>());
+
+		for (int i = 0; i < 100; i++) {
+			this.sendPrivateDeliveryData("b",i+1);
+		}
+	}
+}
+
