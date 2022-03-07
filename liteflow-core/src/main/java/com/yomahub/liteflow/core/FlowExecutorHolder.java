d1421adc53a4ab3d84390d5e4d41ccaad4d7618f
bryan31 <weenyc31@163.com>
2022-03-07 19:18:59 +0800

    提供FlowExecutorHolder的实现

---
+++ liteflow-core/src/main/java/com/yomahub/liteflow/core/FlowExecutorHolder.java
@@ -0,0 +1,28 @@
+package com.yomahub.liteflow.core;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.yomahub.liteflow.exception.FlowExecutorNotInitException;
+import com.yomahub.liteflow.property.LiteflowConfig;
+
+public class FlowExecutorHolder {
+
+    private static FlowExecutor flowExecutor;
+
+    public static FlowExecutor loadInstance(LiteflowConfig liteflowConfig){
+        if (ObjectUtil.isNull(flowExecutor)){
+            flowExecutor = new FlowExecutor(liteflowConfig);
+        }
+        return flowExecutor;
+    }
+
+    public static FlowExecutor loadInstance(){
+        if (ObjectUtil.isNull(flowExecutor)){
+            throw new FlowExecutorNotInitException("flow executor is not initialized yet");
+        }
+        return flowExecutor;
+    }
+
+    public static void clean(){
+        flowExecutor = null;
+    }
+}
