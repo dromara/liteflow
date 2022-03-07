64aff87fb8973e79ec75adc7c19db9f577e324f7
bryan31 <weenyc31@163.com>
2022-03-08 00:39:26 +0800

    补充测试用例

---
+++ liteflow-testcase-nospring/src/test/java/com/yomahub/liteflow/test/tag/cmp/ACmp.java
@@ -0,0 +1,28 @@
+/**
+ * <p>Title: liteflow</p>
+ * <p>Description: 轻量级的组件式流程框架</p>
+ * @author Bryan.Zhang
+ * @email weenyc31@163.com
+ * @Date 2020/4/1
+ */
+package com.yomahub.liteflow.test.tag.cmp;
+
+import com.yomahub.liteflow.core.NodeComponent;
+import com.yomahub.liteflow.entity.data.Slot;
+
+public class ACmp extends NodeComponent {
+
+	@Override
+	public void process() {
+		String testKey = "test";
+
+		Slot slot = this.getSlot();
+		if (slot.getData(testKey) == null){
+			slot.setData(testKey,this.getTag());
+		}else{
+			String s = slot.getData(testKey);
+			s += this.getTag();
+			slot.setData(testKey, s);
+		}
+	}
+}
