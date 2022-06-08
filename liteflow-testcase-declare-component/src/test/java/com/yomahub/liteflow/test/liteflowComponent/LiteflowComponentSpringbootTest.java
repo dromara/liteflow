bb0c3f839553b7f961e743aded0f35a529e8b925
everywhere.z <weenyc31@163.com>
2022-06-08 15:13:34 +0800

    修正测试用例中的包路径问题

--- liteflow-testcase-declare-component/src/test/java/com/yomahub/liteflow/test/liteflowComponent/LiteflowComponentSpringbootTest.java
+++ liteflow-testcase-declare-component/src/test/java/com/yomahub/liteflow/test/liteflowComponent/LiteflowComponentSpringbootTest.java
@@ -1,4 +1,4 @@
-package com.yomahub.liteflow.test.liteflowComponent;
+package com.yomahub.liteflow.test.liteflowcomponent;
 
 import com.yomahub.liteflow.core.FlowExecutor;
 import com.yomahub.liteflow.flow.LiteflowResponse;
@@ -24,7 +24,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(value = "classpath:/liteflowComponent/application.properties")
 @SpringBootTest(classes = LiteflowComponentSpringbootTest.class)
 @EnableAutoConfiguration
-@ComponentScan({"com.yomahub.liteflow.test.liteflowComponent.cmp"})
+@ComponentScan({"com.yomahub.liteflow.test.liteflowcomponent.cmp"})
 public class LiteflowComponentSpringbootTest extends BaseTest {
 
     @Autowired
