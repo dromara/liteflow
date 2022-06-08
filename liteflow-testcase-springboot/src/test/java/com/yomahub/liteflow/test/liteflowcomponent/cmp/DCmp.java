05ef0e28179309a08269fa7d914f637cf3cfed6d
everywhere.z <weenyc31@163.com>
2022-06-08 14:36:48 +0800

    修正测试用例中的包路径问题

--- liteflow-testcase-springboot/src/test/java/com/yomahub/liteflow/test/liteflowcomponent/cmp/DCmp.java
+++ liteflow-testcase-springboot/src/test/java/com/yomahub/liteflow/test/liteflowcomponent/cmp/DCmp.java
@@ -5,7 +5,7 @@
  * @email weenyc31@163.com
  * @Date 2020/4/1
  */
-package com.yomahub.liteflow.test.liteflowcomponent.cmp;
+package com.yomahub.liteflow.test.liteflowComponent.cmp;
 
 import com.yomahub.liteflow.annotation.LiteflowComponent;
 import com.yomahub.liteflow.core.NodeComponent;
