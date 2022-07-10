211e58e53192a64b90bf4ea2a7fea58de3a7c05d
tangkc <1016771049@qq.com>
2022-07-10 23:44:34 +0800

    enhancement #I5GBXI 支持EL中任意地方的注释

---
+++ liteflow-testcase-el/liteflow-testcase-el-nospring/src/test/java/com/yomahub/liteflow/test/note/LiteflowNodeTest.java
@@ -0,0 +1,33 @@
+package com.yomahub.liteflow.test.note;
+
+import com.yomahub.liteflow.core.FlowExecutor;
+import com.yomahub.liteflow.core.FlowExecutorHolder;
+import com.yomahub.liteflow.flow.LiteflowResponse;
+import com.yomahub.liteflow.property.LiteflowConfig;
+import com.yomahub.liteflow.test.BaseTest;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 测试注释
+ */
+public class LiteflowNodeTest extends BaseTest {
+
+	private static FlowExecutor flowExecutor;
+
+	@BeforeClass
+	public static void init(){
+		LiteflowConfig config = new LiteflowConfig();
+		config.setRuleSource("note/flow.el.xml");
+		flowExecutor = FlowExecutorHolder.loadInstance(config);
+	}
+
+	// 测试注释
+	@Test
+	public void testAsyncFlow1() {
+		LiteflowResponse response = flowExecutor.execute2Resp("chain1", "it's a base request");
+		Assert.assertTrue(response.isSuccess());
+		Assert.assertEquals("a==>b==>c==>b",response.getExecuteStepStr());
+	}
+}
