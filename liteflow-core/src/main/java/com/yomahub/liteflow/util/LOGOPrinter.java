587943ca1acb0ad2eecd70f4dc43a2049bd23f2b
everywhere.z <weenyc31@163.com>
2022-08-12 16:30:42 +0800

    版本号更新为v2.8.4

--- liteflow-core/src/main/java/com/yomahub/liteflow/util/LOGOPrinter.java
+++ liteflow-core/src/main/java/com/yomahub/liteflow/util/LOGOPrinter.java
@@ -13,7 +13,7 @@ public class LOGOPrinter {
 	/**
 	 * LiteFlow 当前版本号
 	 */
-	private static final String VERSION_NO = "v2.8.3";
+	private static final String VERSION_NO = "v2.8.4";
 
 	public static void print() {
 		StringBuilder str = new StringBuilder("\n");
