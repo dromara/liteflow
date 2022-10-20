e584527d86cfb5a3bd70775fa858699d3dc1495c
everywhere.z <weenyc31@163.com>
2022-10-20 16:03:41 +0800

    feature #I5WNMG 脚本组件支持javascript的语法

--- liteflow-testcase-el/liteflow-testcase-el-script-groovy-springboot/src/test/java/com/yomahub/liteflow/test/script/groovy/LiteflowXmlScriptGroovyELTest.java
+++ liteflow-testcase-el/liteflow-testcase-el-script-groovy-springboot/src/test/java/com/yomahub/liteflow/test/script/groovy/LiteflowXmlScriptGroovyELTest.java
@@ -48,7 +48,7 @@ public class LiteflowXmlScriptGroovyELTest extends BaseTest {
     public void testScript2() {
         LiteflowResponse response = flowExecutor.execute2Resp("chain2", "arg");
         Assert.assertTrue(response.isSuccess());
-        Assert.assertEquals("d==>s2[条件脚本]==>a", response.getExecuteStepStr());
+        Assert.assertEquals("d==>s2[选择脚本]==>a", response.getExecuteStepStr());
     }
 
     //测试脚本的热重载
@@ -57,7 +57,7 @@ public class LiteflowXmlScriptGroovyELTest extends BaseTest {
         //根据配置，加载的应该是flow.xml，执行原来的规则
         LiteflowResponse responseOld = flowExecutor.execute2Resp("chain2", "arg");
         Assert.assertTrue(responseOld.isSuccess());
-        Assert.assertEquals("d==>s2[条件脚本]==>a", responseOld.getExecuteStepStr());
+        Assert.assertEquals("d==>s2[选择脚本]==>a", responseOld.getExecuteStepStr());
         //更改规则，重新加载，更改的规则内容从flow_update.xml里读取，这里只是为了模拟下获取新的内容。不一定是从文件中读取
         String newContent = ResourceUtil.readUtf8Str("classpath: /xml-script/flow_update.el.xml");
         //进行刷新
