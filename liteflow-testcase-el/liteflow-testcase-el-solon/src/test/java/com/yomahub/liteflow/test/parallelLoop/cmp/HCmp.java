a5fc3869cf06a1e8183a7baff5f555bbe7225d60
zy <953725892@qq.com>
2023-07-14 13:43:45 +0800

    feature #I7HJFX 完成solon场景下的测试用例编写及测试

---
+++ liteflow-testcase-el/liteflow-testcase-el-solon/src/test/java/com/yomahub/liteflow/test/parallelLoop/cmp/HCmp.java
@@ -0,0 +1,18 @@
+package com.yomahub.liteflow.test.parallelLoop.cmp;
+
+import com.yomahub.liteflow.core.NodeComponent;
+import com.yomahub.liteflow.slot.DefaultContext;
+import org.noear.solon.annotation.Component;
+
+
+@Component("h")
+public class HCmp extends NodeComponent{
+
+    @Override
+    public void process() {
+        DefaultContext context = this.getFirstContextBean();
+        context.setData("threadName", Thread.currentThread().getName());
+        System.out.println("HCmp executed!");
+    }
+
+}
---
+++ liteflow-testcase-el/liteflow-testcase-el-solon/src/test/java/com/yomahub/liteflow/test/parallelLoop/cmp/ZCmp.java
@@ -0,0 +1,21 @@
+package com.yomahub.liteflow.test.parallelLoop.cmp;
+
+import com.yomahub.liteflow.core.NodeWhileComponent;
+import com.yomahub.liteflow.slot.DefaultContext;
+import org.noear.solon.annotation.Component;
+
+@Component("z")
+public class ZCmp extends NodeWhileComponent {
+
+    @Override
+    public boolean processWhile() throws Exception {
+        DefaultContext context = this.getFirstContextBean();
+        String key = "test";
+        if (context.hasData(key)) {
+            int count = context.getData("test");
+            return count < 5;
+        } else {
+            return true;
+        }
+    }
+}
