afb1e16692cb7ff218232aea644cf8630a9b2662
everywhere.z <weenyc31@163.com>
2023-08-30 12:07:44 +0800

    更新banner打印

--- liteflow-core/src/main/java/com/yomahub/liteflow/util/LOGOPrinter.java
+++ liteflow-core/src/main/java/com/yomahub/liteflow/util/LOGOPrinter.java
@@ -31,10 +31,8 @@ public class LOGOPrinter {
 		str.append("		| |___ | |  | | | |__|_____|  _| | |__| |_| |\\ V  V /  \n");
 		str.append("		|_____|___| |_| |_____|    |_|   |_____\\___/  \\_/\\_/   \n\n");
 		str.append("		Version: " + VERSION_NO + "\n");
-		str.append("		新一代轻量且强大的编排式规则引擎。\n");
-		str.append("		基于开源社区文化，社区驱动型开源框架。\n");
-		str.append("		Small but powerful rules engine.\n");
-		str.append("		官网地址：https://liteflow.yomahub.com/\n");
+		str.append("		Keep on the light side.\n");
+		str.append("		website：https://liteflow.cc\n");
 		str.append("		wechat：bryan_31\n");
 		str.append(
 				"================================================================================================\n");
