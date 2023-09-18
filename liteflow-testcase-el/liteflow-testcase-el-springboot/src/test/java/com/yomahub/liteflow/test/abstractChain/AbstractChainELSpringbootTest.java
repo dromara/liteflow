d72fb33a813a27916360e3e6b5e01d8d2be09ad9
zy <953725892@qq.com>
2023-09-18 21:11:07 +0800

    test #I7SVZF 添加XML中Chain的测试用例

---
+++ liteflow-testcase-el/liteflow-testcase-el-springboot/src/test/java/com/yomahub/liteflow/test/abstractChain/AbstractChainELSpringbootTest.java
@@ -0,0 +1,36 @@
+package com.yomahub.liteflow.test.abstractChain;
+
+import com.yomahub.liteflow.core.FlowExecutor;
+import com.yomahub.liteflow.flow.LiteflowResponse;
+import com.yomahub.liteflow.test.BaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.annotation.Resource;
+
+/**
+ * springboot环境EL常规的例子测试
+ *
+ * @author Bryan.Zhang
+ */
+@TestPropertySource(value = "classpath:/abstractChain/application.properties")
+@SpringBootTest(classes = AbstractChainELSpringbootTest.class)
+@EnableAutoConfiguration
+@ComponentScan({ "com.yomahub.liteflow.test.abstractChain.cmp" })
+public class AbstractChainELSpringbootTest extends BaseTest {
+
+	@Resource
+	private FlowExecutor flowExecutor;
+
+	// XML文件单继承测试
+	@Test
+	public void test1() throws Exception {
+		LiteflowResponse response = flowExecutor.execute2Resp("implA", "arg");
+		Assertions.assertTrue(response.isSuccess());
+		Assertions.assertEquals("a==>b==>c==>d==>f==>j", response.getExecuteStepStrWithoutTime());
+	}
+}
