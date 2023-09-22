ca9449f531036fb572284dd1e1820e077e931759
luoyi <972849752@qq.com>
2023-09-22 23:38:58 +0800

    enhancement #I821F1 增加测试案例，调整注释

---
+++ liteflow-testcase-el/liteflow-testcase-el-nospring/src/test/java/com/yomahub/liteflow/test/subflow/endlessLopp/FlowYMLTest.java
@@ -0,0 +1,29 @@
+package com.yomahub.liteflow.test.subflow.endlessLopp;
+
+import com.yomahub.liteflow.core.FlowExecutorHolder;
+import com.yomahub.liteflow.exception.CyclicDependencyException;
+import com.yomahub.liteflow.property.LiteflowConfig;
+import com.yomahub.liteflow.property.LiteflowConfigGetter;
+import com.yomahub.liteflow.test.BaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 测试 yml 文件情况下 chain 死循环逻辑
+ *
+ * @author luo yi
+ * @since 2.11.1
+ */
+public class FlowYMLTest extends BaseTest {
+
+	// 测试 chain 死循环
+	@Test
+	public void testChainEndlessLoop() {
+		Assertions.assertThrows(CyclicDependencyException.class, () -> {
+			LiteflowConfig config = LiteflowConfigGetter.get();
+			config.setRuleSource("subflow/endlessLoop/flow.el.yml");
+			FlowExecutorHolder.loadInstance(config);
+		});
+	}
+
+}
