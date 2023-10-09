f2a8a494dd9bed61fd567c844d2effc59eba9e62
everywhere.z <weenyc31@163.com>
2023-10-09 20:09:39 +0800

    简化组件降级的Node装配代理逻辑

--- liteflow-core/src/main/java/com/yomahub/liteflow/builder/el/operator/NodeOperator.java
+++ liteflow-core/src/main/java/com/yomahub/liteflow/builder/el/operator/NodeOperator.java
@@ -2,12 +2,9 @@ package com.yomahub.liteflow.builder.el.operator;
 
 import com.yomahub.liteflow.builder.el.operator.base.BaseOperator;
 import com.yomahub.liteflow.builder.el.operator.base.OperatorHelper;
-import com.yomahub.liteflow.exception.ELParseException;
 import com.yomahub.liteflow.flow.FlowBus;
 import com.yomahub.liteflow.flow.element.FallbackNodeProxy;
 import com.yomahub.liteflow.flow.element.Node;
-import com.yomahub.liteflow.property.LiteflowConfig;
-import com.yomahub.liteflow.property.LiteflowConfigGetter;
 
 /**
  * EL规则中的node的操作符
@@ -27,12 +24,6 @@ public class NodeOperator extends BaseOperator<Node> {
             // 找到对应节点
             return FlowBus.getNode(nodeId);
         } else {
-            // 检查是否开启了组件降级功能
-            LiteflowConfig liteflowConfig = LiteflowConfigGetter.get();
-            Boolean enable = liteflowConfig.getFallbackCmpEnable();
-            if (!enable) {
-                throw new ELParseException("The fallback component is disabled");
-            }
             // 生成代理节点
             return new FallbackNodeProxy(nodeId);
         }
