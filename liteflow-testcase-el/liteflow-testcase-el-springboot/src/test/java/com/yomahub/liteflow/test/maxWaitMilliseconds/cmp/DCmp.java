0ed020d498d3293cd506d24d691b58c0640b7732
Kugaaa <grznz2012@163.com>
2023-11-03 14:41:20 +0800

    feature 超时时间支持毫秒 springboot UT

---
+++ liteflow-testcase-el/liteflow-testcase-el-springboot/src/test/java/com/yomahub/liteflow/test/maxWaitMilliseconds/cmp/DCmp.java
@@ -0,0 +1,24 @@
+package com.yomahub.liteflow.test.maxWaitMilliseconds.cmp;
+
+import com.yomahub.liteflow.annotation.LiteflowComponent;
+import com.yomahub.liteflow.core.NodeComponent;
+import com.yomahub.liteflow.slot.DefaultContext;
+
+@LiteflowComponent("d")
+public class DCmp  extends NodeComponent {
+
+    public static final String CONTENT_KEY = "testKey";
+
+    @Override
+    public void process() {
+        try {
+            Thread.sleep(500);
+            DefaultContext contextBean = this.getFirstContextBean();
+            contextBean.setData(CONTENT_KEY,"value");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("DCmp executed!");
+    }
+}
