b7204aef69248836f3617c7e0ad0ef288d5b0cf0
everywhere.z <weenyc31@163.com>
2024-09-23 14:09:08 +0800

    代码优化

--- liteflow-script-plugin/liteflow-script-aviator/src/main/java/com/yomahub/liteflow/script/aviator/AviatorScriptExecutor.java
+++ liteflow-script-plugin/liteflow-script-aviator/src/main/java/com/yomahub/liteflow/script/aviator/AviatorScriptExecutor.java
@@ -1,9 +1,7 @@
 package com.yomahub.liteflow.script.aviator;
 
-import cn.hutool.core.collection.ListUtil;
 import com.yomahub.liteflow.enums.ScriptTypeEnum;
 import com.yomahub.liteflow.script.jsr223.JSR223ScriptExecutor;
-import java.util.List;
 
 public class AviatorScriptExecutor extends JSR223ScriptExecutor {
 
