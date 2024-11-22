9004cd274af618f8d515830f321251340f4cc7e9
everywhere.z <weenyc31@163.com>
2024-11-22 19:17:14 +0800

    优化测试用例

--- liteflow-testcase-el/liteflow-testcase-el-script-qlexpress-springboot/src/test/java/com/yomahub/liteflow/test/script/qlexpress/LiteflowXmlScriptQLExpressELParseModeTest.java
+++ liteflow-testcase-el/liteflow-testcase-el-script-qlexpress-springboot/src/test/java/com/yomahub/liteflow/test/script/qlexpress/LiteflowXmlScriptQLExpressELParseModeTest.java
@@ -69,39 +69,4 @@ public class LiteflowXmlScriptQLExpressELParseModeTest extends BaseTest {
 			}
 		}
 	}
-
-	// 测试普通脚本节点
-	@Test
-	public void testScript1() {
-		LiteflowResponse response = flowExecutor.execute2Resp("chain1", "arg");
-		DefaultContext context = response.getFirstContextBean();
-		Assertions.assertTrue(response.isSuccess());
-		Assertions.assertEquals(Integer.valueOf(6), context.getData("s1"));
-	}
-
-	// 测试条件脚本节点
-	@Test
-	public void testScript2() {
-		LiteflowResponse response = flowExecutor.execute2Resp("chain2", "arg");
-		Assertions.assertTrue(response.isSuccess());
-		Assertions.assertEquals("d==>s2[条件脚本]==>b", response.getExecuteStepStr());
-	}
-
-	@Test
-	public void testScript3() throws Exception {
-		// 根据配置，加载的应该是flow.xml，执行原来的规则
-		LiteflowResponse responseOld = flowExecutor.execute2Resp("chain2", "arg");
-		Assertions.assertTrue(responseOld.isSuccess());
-		Assertions.assertEquals("d==>s2[条件脚本]==>b", responseOld.getExecuteStepStr());
-		// 更改规则，重新加载，更改的规则内容从flow_update.xml里读取，这里只是为了模拟下获取新的内容。不一定是从文件中读取
-		String newContent = ResourceUtil.readUtf8Str("classpath: /xml-script/flow_update.el.xml");
-		// 进行刷新
-		FlowBus.refreshFlowMetaData(FlowParserTypeEnum.TYPE_EL_XML, newContent);
-
-		// 重新执行chain2这个链路，结果会变
-		LiteflowResponse responseNew = flowExecutor.execute2Resp("chain2", "arg");
-		Assertions.assertTrue(responseNew.isSuccess());
-		Assertions.assertEquals("d==>s2[条件脚本_改]==>a==>s3[普通脚本_新增]", responseNew.getExecuteStepStr());
-	}
-
 }
