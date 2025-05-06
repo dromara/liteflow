97af056acff0adbc4266ecd60f6567c837216cf9
everywhere.z <weenyc31@163.com>
2025-05-06 14:40:37 +0800

    bug #IC0254 node在设置data数据时，含有中文时会解析成Unicode转义字符

--- liteflow-el-builder/src/main/java/com/yomahub/liteflow/builder/el/ELWrapper.java
+++ liteflow-el-builder/src/main/java/com/yomahub/liteflow/builder/el/ELWrapper.java
@@ -4,7 +4,7 @@ import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.StrUtil;
 import com.yomahub.liteflow.builder.el.vo.RetryELVo;
 import com.yomahub.liteflow.util.JsonUtil;
-import org.apache.commons.lang.StringEscapeUtils;
+import com.yomahub.liteflow.util.SelectiveJavaEscaper;
 
 import java.util.*;
 
@@ -142,7 +142,7 @@ public abstract class ELWrapper {
      * @return {@link ELWrapper}
      */
     protected ELWrapper data(String dataName, Object object){
-        setData(StrUtil.format("\"{}\"",StringEscapeUtils.escapeJava(JsonUtil.toJsonString(object))));
+        setData(StrUtil.format("\"{}\"", SelectiveJavaEscaper.escape(JsonUtil.toJsonString(object))));
         setDataName(dataName);
         return this;
     }
@@ -155,7 +155,7 @@ public abstract class ELWrapper {
      * @return {@link ELWrapper}
      */
     protected ELWrapper data(String dataName, String jsonString){
-        setData(StrUtil.format("\"{}\"",StringEscapeUtils.escapeJava(jsonString)));
+        setData(StrUtil.format("\"{}\"",SelectiveJavaEscaper.escape(jsonString)));
         setDataName(dataName);
         return this;
     }
@@ -168,7 +168,7 @@ public abstract class ELWrapper {
      * @return {@link ELWrapper}
      */
     protected ELWrapper data(String dataName, Map<String, Object> jsonMap){
-        setData(StrUtil.format("\"{}\"", StringEscapeUtils.escapeJava(JsonUtil.toJsonString(jsonMap))));
+        setData(StrUtil.format("\"{}\"", SelectiveJavaEscaper.escape(JsonUtil.toJsonString(jsonMap))));
         setDataName(dataName);
         return this;
     }
@@ -242,14 +242,14 @@ public abstract class ELWrapper {
             elContext.append(StrUtil.format(".id(\"{}\")", this.getId()));
         }
         if(this.getTag() != null){
-            elContext.append(StrUtil.format(".tag(\"{}\")", StringEscapeUtils.escapeJava(this.getTag())));
+            elContext.append(StrUtil.format(".tag(\"{}\")", SelectiveJavaEscaper.escape(this.getTag())));
         }
         if(this.getData() != null){
             elContext.append(StrUtil.format(".data({})", this.getDataName()));
             paramContext.append(StrUtil.format("{} = {}", this.getDataName(), this.getData())).append(";\n");
         }
         if(MapUtil.isNotEmpty(this.getBindData())){
-            this.getBindData().forEach((key, value) -> elContext.append(StrUtil.format(".bind(\"{}\", \"{}\")", key, StringEscapeUtils.escapeJava(value))));
+            this.getBindData().forEach((key, value) -> elContext.append(StrUtil.format(".bind(\"{}\", \"{}\")", key, SelectiveJavaEscaper.escape(value))));
         }
         if(this.getMaxWaitSeconds() != null){
             elContext.append(StrUtil.format(".maxWaitSeconds({})", String.valueOf(this.getMaxWaitSeconds())));
